a6c40907e5cc1a4ca9f25237e78c259a968242cb
Timothy Reuter <timothyjreuter@gmail.com>
2018-08-05 16:59:18 -0400

    Added basic main menu scene with functioning play and quit buttons.
    Organized UI scripts into their own folder.

---
+++ GoneFishing/Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("Level");
+	}
+
+	public void Options()
+	{
+
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+	
+}
